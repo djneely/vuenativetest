d7ea178afafdf4b0ba45ccc1683e324bb03bdd37
Derek Neely <derek@v4development.com>
2021-02-14 13:31:31 -0500

    Initial commit

---
+++ App.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="container">
+    <HeaderTest />
+    <text class="text-color-primary">{{ message }}</text>
+    <button title="Press me!" @press="exclaim" />
+  </view>
+</template>
+
+<script>
+import HeaderTest from "./HeaderTest";
+
+export default {
+  components: { HeaderTest },
+  data() {
+    return {
+      message: "Hello World"
+    };
+  },
+  methods: {
+    exclaim() {
+      this.message += "!";
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+}
+.text-color-primary {
+  color: blue;
+  font-size: 30px;
+}
+</style>
---
+++ components/HeaderTest.vue
@@ -0,0 +1,13 @@
+<template>
+<div>Hello Header</div>
+</template>
+
+<script>
+export default {
+  name: "HeaderTest.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
